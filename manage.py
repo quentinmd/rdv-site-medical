81021edcf765286d2c1a277dfab6057bab7b11fd
quentinmd <117288247+quentinmd@users.noreply.github.com>
2024-02-12 19:20:14 +0100

    Delete manage.py

--- manage.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from flask_migrate import Migrate
-from serveur_3 import app, db  # Import your Flask app and SQLAlchemy db instance
-
-migrate = Migrate(app, db)
-
-if __name__ == '__main__':
-    app.run()
